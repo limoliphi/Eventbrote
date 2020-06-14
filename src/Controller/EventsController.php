5c83cf1bcd10ff096d4b227fac37d1c544452621
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-06-14 08:45:59 +0200

    add show page links and roads

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +11,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class EventsController extends AbstractController
 {
     /**
-     * @Route("/events", methods={"GET"})
+     * @Route("/", name="home", methods={"GET"})
+     * @Route("/events", name="events_index", methods={"GET"})
      */
     public function index(EventRepository $eventRepository): Response
     {
@@ -22,11 +24,10 @@ class EventsController extends AbstractController
     }
 
     /**
-     * @Route("/events/{id}", requirements={"id": "\d+"}, methods={"GET"})
+     * @Route("/events/{id}", name="events_show", requirements={"id": "\d+"}, methods={"GET"})
      */
-    public function show(int $id, EventRepository $eventRepository)
+    public function show(EventRepository $eventRepository, Event $event)
     {
-        $event = $eventRepository->find($id);
 
         return $this->render('events/show.html.twig', compact('event'));
     }
