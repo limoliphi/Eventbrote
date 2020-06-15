1a3f9aa3e679ad790776036f4c4dcd2bd84dee75
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-06-15 08:36:39 +0200

    add edit form type

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use App\Entity\Event;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,9 +30,28 @@ class EventsController extends AbstractController
     /**
      * @Route("/events/{id}", name="events_show", requirements={"id": "\d+"}, methods={"GET"})
      */
-    public function show(EventRepository $eventRepository, Event $event)
+    public function show(Event $event)
     {
 
         return $this->render('events/show.html.twig', compact('event'));
+    }
+
+    /**
+     * @Route("/events/{id}/edit", name="events_edit", requirements={"id": "\d+"}, methods={"GET"})
+     */
+    public function edit(Event $event)
+    {
+        $form = $this->createFormBuilder($event)
+            ->add('name', TextType::class)
+            ->add('location', TextType::class)
+            ->add('price', NumberType::class, ['html5' => true, 'scale' => 2])
+            ->add('description', TextareaType::class, ['attr' => ['rows' => 5]])
+            ->add('startAt', DateTimeType::class)
+            ->getForm();
+
+        return $this->render('events/edit.html.twig', [
+            'event' => $event,
+            'form' => $form->createView()
+        ]);
     }
 }
